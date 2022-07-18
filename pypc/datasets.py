932bf899bada93832bd756a42d792638ab147f43
Nick Hay <83538768+lasermanick@users.noreply.github.com>
2022-07-18 09:21:27 +0100

    datasets.py documentation
    Added/edited doc strings

--- pypc/datasets.py
+++ pypc/datasets.py
@@ -27,8 +27,8 @@ class MNIST(datasets.MNIST):
 
     def __getitem__(self, index):
         """
-        Return image (data) and label (target) with image converted from 28x28 to 784x1 and label converted to one-hot
-        encoding (optionally scaled)
+        Return image (data) and label (target) with image converted from (1,28,28) to (784,) and label converted to
+        one-hot encoding (optionally scaled)
 
         :param index: Index
         :return: image, label
@@ -147,6 +147,14 @@ class FashionMNIST(datasets.FashionMNIST):
 
 
 def get_dataloader(dataset, batch_size):
+    """
+    Create PyTorch DataLoader for given dataset and batch size, perform pre-processing to move data onto the selected
+    cpu/cuda device with dtype=torch.float32, and return a list containing samples and labels
+
+    :param dataset: PyTorch Dataset
+    :param batch_size: Batch size
+    :return: List of tuples with index 0 containing samples and index 1 containing labels
+    """
     dataloader = data.DataLoader(dataset, batch_size, shuffle=True, drop_last=True)
     return list(map(_preprocess_batch, dataloader))
 
@@ -173,6 +181,12 @@ def plot_imgs(img_preds, path):
 
 
 def _preprocess_batch(batch):
+    """
+    Pre-process a batch to move data onto the selected cpu/cuda device with dtype=torch.float32
+
+    :param batch: List of Tensor objects with index 0 containing samples and index 1 containing labels
+    :return: Pre-processed batch as tuple of Tensor objects
+    """
     batch[0] = utils.set_tensor(batch[0])
     batch[1] = utils.set_tensor(batch[1])
     return (batch[0], batch[1])
@@ -194,14 +208,36 @@ def _get_transform(normalize=True, mean=0.0, std=1.0):
 
 
 def _one_hot(labels, n_classes=10):
+    """
+    Convert categorical label to one-hot encoding (trick is to index an identity matrix) NOTE: Only used for individual
+    labels so consider changing parameter name to 'label')
+
+    :param labels: Categorical label
+    :param n_classes: Number of classes (categories)
+    :return: One-hot encoded label
+    """
     arr = torch.eye(n_classes)
     return arr[labels]
 
 
 def _scale(targets, factor):
+    """
+    Scale one-hot labels (targets) according to:
+    scaled = 0.5 + factor x (original - 0.5)
+    (e.g. scale=1 => 0 or 1, scale=0.5 => 0.25 or 0.75, scale=0.1 => 0.45 or 0.55)
+
+    :param targets: Labels
+    :param factor: Scale factor
+    :return: Scaled labels
+    """
     return targets * factor + 0.5 * (1 - factor) * torch.ones_like(targets)
 
 
 def _to_vector(batch):
+    """
+    Convert batch of 2D images to vector format NOTE: Currently only used for single images so naming is confusing
+    :param batch: Image or batch of images
+    :return: Image or batch of images in vector format
+    """
     batch_size = batch.size(0)
     return batch.reshape(batch_size, -1).squeeze()
